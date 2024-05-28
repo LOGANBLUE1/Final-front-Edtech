0f2df16b802c1c3f46e9c8e978f765181f4746ed
aditya <mrpareekaditya@gmail.com>
2024-05-28 11:08:26 +0530

    cart added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
+// Redux
+import { useDispatch, useSelector } from "react-redux"
 // React Router
-import {Route, Routes } from "react-router-dom";
+import {Route, Routes, useNavigate } from "react-router-dom";
 // Components
 import Navbar from "./components/common/Navbar"
 import OpenRoute from "./components/core/Auth/OpenRoute"
+import EnrolledCourses from "./components/core/Dashboard/EnrolledCourses"
 import PrivateRoute from "./components/core/Auth/PrivateRoute"
 import ForgotPassword from "./pages/ForgotPassword"
 import About from "./pages/About"
+import Cart from "./components/core/Dashboard/Cart"
 import Contact from "./pages/Contact"
 import Dashboard from "./pages/Dashboard"
 import Error from "./pages/Error"
 import MyProfile from "./components/core/Dashboard/MyProfile";
+// import Settings from "./components/core/Dashboard/Settings"
 // Pages
 import Home from "./pages/Home"
 import Login from "./pages/Login"
@@ -18,8 +23,13 @@ import Signup from "./pages/Signup"
 import UpdatePassword from "./pages/UpdatePassword"
 import VerifyEmail from "./pages/VerifyEmail"
 // import ViewCourse from "./pages/ViewCourse"
+import { ACCOUNT_TYPE } from "./utils/constants"
 
 function App() {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const { user } = useSelector((state) => state.profile)
+
   return (
    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
     <Navbar/>
@@ -80,6 +90,18 @@ function App() {
         >  
           {/* Route for all users */}
           <Route path="dashboard/my-profile" element={<MyProfile />} />
+          {/* <Route path="dashboard/Settings" element={<Settings />} /> */}
+
+           {/* Route only for Students */}
+           {user?.accountType === ACCOUNT_TYPE.STUDENT && (
+            <>
+              <Route
+                path="dashboard/enrolled-courses"
+                element={<EnrolledCourses />}
+              />
+              <Route path="/dashboard/cart" element={<Cart />} />
+            </>
+          )}
         </Route>
 
         
